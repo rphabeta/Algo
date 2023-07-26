9c96fd46d2e02629e1fb1432e6565f792c0ad641
rphabeta <134386253+rphabeta@users.noreply.github.com>
2023-07-26 23:52:25 +0900

    for문을 이용한 완전탐색

---
+++ ExhaustiveSearch/ExhaustiveSearchFor.cs
@@ -0,0 +1,28 @@
+/*
+   민수는 2400이란 숫자를 좋아한다. 태민이는 민수의 수를 만들고자 하는데 민수의 수란 2400이 들어간 수를 말한다.
+   첫 번째 민수의 수는 2400이고 두 번째 민수의 수는 12400, 세번째 민수의 수는 224000이다.
+   N이 입력을 주어졌을 때 N번째 민수의 수를 구하라. N은 300이하로 주어진다.
+*/
+using System;
+namespace ExhaustiveSearchFor
+{
+    class Program
+    {
+        static int n, cnt;
+        const int INF = 1000000;
+
+        static void Main(string[] args)
+        {
+            n = int.Parse(Console.ReadLine());
+            for (int i = 2400; i < INF; i++)
+            {
+                string a = i.ToString();
+                if (a.Contains("2400"))
+                {
+                    cnt++;
+                    Console.WriteLine(cnt + " : " + a);
+                }
+            }
+        }
+    }
+}
